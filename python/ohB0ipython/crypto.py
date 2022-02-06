0ccd4741d5cee2be014052986330e26f486397af
ViCExx <98494599+ViCExx@users.noreply.github.com>
2022-02-06 00:12:02 -0600

    ETH

--- python/ohB0ipython/crypto.py
+++ python/ohB0ipython/crypto.py
@@ -6,14 +6,22 @@ def get_price(symbol):
   parameters = {
     'symbol': symbol
   }
-
+  
   headers = {
   'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': '6292efb6-82b6-4806-80c1-e54aa4044ed' #missing the 9
+  'X-CMC_PRO_API_KEY': '<get from coinmarket>'
   }
   response = requests.get(url, params=parameters, headers=headers)
 
-  return response.json().get('data', {}).get(symbol, {}).get('quote', {}).get('USD', {}).get('price',{})
-  #return response.json().get('data', {})
+  #return response.json().get('data').get(symbol).get('quote').get('USD').get('price')
+  return response.json().get('data', {}).get(symbol,{}).get('quote',{}).get('USD',{'price','percent_change_1h'})
+Tickers = ['BTC', 'ETH']
+#Tickers = ['BTC', 'ETH', 'DOT', 'XRP', 'DOGE','SHIB', 'ADA', 'SOL']
+for ticker in Tickers:
+  next(iter(ticker))
+  print(ticker, "is: ")
+  pprint.pprint(get_price(ticker))
 
-pprint.pprint(get_price('ETH'))
+  # was very excited to get this working with a list in a for loop.
+  # next project will be to create an input and have it deliver the stats
+  
